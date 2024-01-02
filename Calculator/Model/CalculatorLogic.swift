e9b4c95e59f154e25037b419dc83e6ebb3e6264c
prtadipkhandare111 <pradip.khandare@neutrinotechlabs.com>
2024-01-02 12:21:37 +0530

    initial commit

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,61 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by NTS on 01/01/24.
+//  Copyright © 2024 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic
+{
+    private var number: Double?
+    
+    private var intermediateCalculation : (n1: Double, calcMethod: String)?
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double?{
+        
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("provide right symbol for calculation")
+            }
+        }
+            return nil
+    }
+       
+}
+
+
